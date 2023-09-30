8d26588bde7479a92d35d6c7f6c2d1142b280f4d
Griswald Brooks <griswald.brooks@gmail.com>
2023-09-29 23:52:16 -0400

    Add PlannerManager

--- core/motion_plan.cpp
+++ core/motion_plan.cpp
@@ -1,12 +1,11 @@
-
 #include <memory>
 #include <filesystem>
 #include "plugin_api/motion_planner_interface.h"
 #include "plugin_manager/class_loader.h"
 #include "motion_plan_config.h"
+#include "planner_manager/planner_manager.h"
 
-
-std::vector<RobotState> createMotionPlan(RobotState start, RobotState goal)
+std::shared_ptr<IMotionPlanner> getDefaultMotionPlanner()
 {
   // Ideally, we'd have another object handle getting this path.
   // For demonstrative purposes, we'll just hard code this for now
@@ -14,18 +13,16 @@ std::vector<RobotState> createMotionPlan(RobotState start, RobotState goal)
     library_build_path() + "/simple_motion_planner/libsimple_motion_planner.so"};
   // Load the correct planner. What should the parameter be. In ROS world, we send the package name and base class name
   // So we need something to find the path of .so and the interface name
-  using LoaderType = ClassLoader<IMotionPlanner>;
-  std::shared_ptr<LoaderType> loader = std::make_shared<LoaderType>(dll_path.string());
+  auto loader = ClassLoader<IMotionPlanner>(dll_path.string());
 
   // In ROS world, we send the plugin name which is declared in XML file. Maybe we send in the derived class name?
-  std::shared_ptr<IMotionPlanner> motion_planner = loader->GetInstance();
-
-  // Call the plan function
-  return motion_planner->plan(start, goal);
+  return loader.GetInstance();
 }
 
 int main(){
-  auto start_state = RobotState();
-  auto goal_state = RobotState();
-  auto motion_plan = createMotionPlan(start_state, goal_state);
+  const auto start_state = RobotState();
+  const auto goal_state = RobotState();
+  const auto motion_planner = getDefaultMotionPlanner();
+  const auto planner_manager = PlannerManager(motion_planner);
+  const auto motion_plan = planner_manager.createMotionPlan(start_state, goal_state);
 }
